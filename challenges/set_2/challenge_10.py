a0a82e65b358ee2fd2427cda27a0133e8fb23b74
Kaspar Hageman <kaspar.hageman@nedap.com>
2017-06-13 17:46:02 +0200

    Challenge 10

--- challenges/set_2/challenge_10.py
+++ challenges/set_2/challenge_10.py
@@ -1,15 +1,13 @@
-from util import crypto, file
+from util import crypto, file, convert
 
 
 def solve():
-    ct_original = file.read("set_2/challenge_10")
+    ct_original = convert.from_base64(file.read("set_2/challenge_10"))
     key = b"YELLOW SUBMARINE"
     iv = bytes(16)
 
     pt = crypto.aes_cbc_decrypt(ct_original, key, iv)
-    ct_self = crypto.aes_cbc_encrypt(pt, key, iv)
-
-    print(ct_original == ct_self)
+    print(convert.to_string(pt))
 
 if __name__ == "__main__":
     solve()
