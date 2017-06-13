c53bdd41e94cb44d0bfbd78b16a5ebb51bd93088
Kaspar Hageman <kaspar.hageman@nedap.com>
2017-06-13 15:41:46 +0200

    Challenge 7

---
+++ challenges/challenge_7.py
@@ -0,0 +1,19 @@
+import util.file as file
+import util.convert as convert
+from Crypto.Cipher import AES
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def solve():
+    ct = convert.from_base64(file.read("challenge_7"))
+    key = "YELLOW SUBMARINE"
+
+    aes = AES.new(key, mode=AES.MODE_ECB)
+    pt = aes.decrypt(ct)
+    pp.pprint(pt)
+
+
+if __name__ == "__main__":
+        solve()
