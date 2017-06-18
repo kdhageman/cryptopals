556cbcb3e3f7fbc79f66da6f361ccc8889dd3b8c
Kaspar Hageman <kaspar.hageman@nedap.com>
2017-06-18 19:48:59 +0200

    Challenge 14

--- challenges/set_2/challenge_12.py
+++ challenges/set_2/challenge_12.py
@@ -1,59 +1,78 @@
-from exception.cryptoexception import CryptoException
+from exception.exceptions import CryptoException, NotFoundException
 from util import crypto, convert
 from util.modes import Mode
 
 BYTE = b'A'
 NULL_BYTE = b''
 
+
 def solve():
-    crypto.set_global_key()
+    crypto.set_global_key()  # set the global encryption key
     try:
-        bs = detect_block_size()
+        s_block = detect_s_block()  # find the block size of the cipher
     except CryptoException as e:
         print(e.args[0])
         return
 
-    if crypto.detect_mode(crypto.encrypt_oracle_consistent) != Mode.ECB:
+    if crypto.detect_mode(crypto.encrypt_oracle_consistent_12) != Mode.ECB:  # validate that ECB mode is used
         print("Error!")
         return
 
-    unknown = get_unknown_string(bs)
+    try:
+        unknown = get_unknown_string(s_block)  # retrieve the unknown string
+    except NotFoundException as e:
+        print(e.args[0])
+        return
+
     print(convert.to_string(unknown))
 
 
-def get_unknown_string(bs):
+def get_unknown_string(s_block):
+    """
+    Returns the unknown string that is appended in the encrypt_oracle_consistent_12 function
+    :param s_block: block size
+    :return: the unknown string
+    """
     unknown = NULL_BYTE
 
     block_ctr = 0
 
-    for k in range(40):
-        offset = block_ctr * bs
-        for j in range(16):
-            base_pt = (BYTE * (bs - (j + 1)))
-            base_key_pt = base_pt + unknown
-            base_ct = crypto.encrypt_oracle_consistent(base_pt)
+    for k in range(64):  # loop over (up to 64) blocks; answers is shorter but make sure that loop terminates
+        offset = block_ctr * s_block
+        for j in range(s_block):  # loop over each byte in the block
+            incomplete_pt = (BYTE * (s_block - (j + 1)))
+            incomplete_ct = crypto.encrypt_oracle_consistent_12(incomplete_pt)
 
-            ct_dict = {}
-            for i in range(256):
-                pt = base_key_pt + bytes([i])
-                ct = crypto.encrypt_oracle_consistent(pt)
-                ct_dict[ct[offset:offset + bs]] = i
+            base_pt = incomplete_pt + unknown
 
-            if base_ct[offset:offset + bs] not in ct_dict:
+            ct_dict = {}  # dictionary that holds every cipher text
+            for i in range(256):  # create every possible cipher text for the full block
+                pt = base_pt + bytes([i])
+                ct = crypto.encrypt_oracle_consistent_12(pt)
+                ct_dict[ct[offset:offset + s_block]] = i
+
+            if incomplete_ct[offset:offset + s_block] not in ct_dict:  # cannot match any of the 256 possible bytes; end has been found
                 return unknown
-            unknown += bytes([ct_dict[base_ct[offset:offset + bs]]])
+            unknown += bytes(
+                [ct_dict[incomplete_ct[offset:offset + s_block]]])  # add found byte to the end of the unknown string
         block_ctr += 1
+    raise NotFoundException("Failed to find unknown string")
 
 
-def detect_block_size():
+def detect_s_block():
+    """
+    Detects the block size of the encrypt_oracle_consistent function
+    :return:
+    """
     for i in range(1, 128):
         base_pt = BYTE * i
         pt = base_pt + BYTE
-        base_ct = crypto.encrypt_oracle_consistent(base_pt)
-        ct = crypto.encrypt_oracle_consistent(pt)
+        base_ct = crypto.encrypt_oracle_consistent_12(base_pt)
+        ct = crypto.encrypt_oracle_consistent_12(pt)
         if base_ct[0:i] == ct[0:i]:
             return i
     raise CryptoException("Failed to detect block size")
+
 
 if __name__ == "__main__":
     solve()
