ac561fc200ebd293bbb1f93392c3dbab1b7379e2
Kaspar Hageman <kaspar.hageman@nedap.com>
2017-06-20 21:45:17 +0200

    Decorate challenge 15

--- challenges/set_2/challenge_15.py
+++ challenges/set_2/challenge_15.py
@@ -1,14 +1,27 @@
-from util import crypto
+from exception.exceptions import PaddingException
+from util import pkcs7, aes
+from challenges.decorator import challenge, expect
+
+EXPECTED = b"ICE ICE BABY"
 
 
+@challenge(15)
 def solve():
-    s_block = 16
-    inp = b'A' * 8
+    inp = b"ICE ICE BABY\x04\x04\x04\x04"
+    actual = pkcs7.remove(inp, aes.S_BLOCK)
+    expect(actual, EXPECTED)
 
-    inp_padded = crypto.add_padding(inp, s_block)
+    try:
+        inp = b"ICE ICE BABY\x05\x05\x05\x05"
+        actual = pkcs7.remove(inp, aes.S_BLOCK)
+    except PaddingException as e:
+        expect(actual, actual)  # remains unchanged
 
-    outp = crypto.del_padding(inp_padded, s_block)
-    print(outp)
+    try:
+        inp = b"ICE ICE BABY\x01\x02\x03\x04"
+        actual = pkcs7.remove(inp, aes.S_BLOCK)
+    except PaddingException as e:
+        expect(actual, actual)  # remains unchanged
 
 
 if __name__ == "__main__":
