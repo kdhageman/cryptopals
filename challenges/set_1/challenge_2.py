23187aad5e430fd386793aef2020a9677e3b5073
Kaspar Hageman <kaspar.hageman@nedap.com>
2017-06-20 19:17:17 +0200

    Decorate challenge 2

--- challenges/set_1/challenge_2.py
+++ challenges/set_1/challenge_2.py
@@ -1,16 +1,18 @@
 from util import crypto, convert
+from challenges.decorator import challenge, expect
+
+INPUT_A = "1c0111001f010100061a024b53535009181c"
+INPUT_B = "686974207468652062756c6c277320657965"
+EXPECTED = convert.from_hex('746865206b696420646f6e277420706c6179')
 
 
+@challenge(2)
 def solve():
-    input_a = '1c0111001f010100061a024b53535009181c'
-    input_b = '686974207468652062756c6c277320657965'
-    expected = convert.from_hex('746865206b696420646f6e277420706c6179')
-
-    a_bytes = convert.from_hex(input_a)
-    b_bytes = convert.from_hex(input_b)
+    a_bytes = convert.from_hex(INPUT_A)
+    b_bytes = convert.from_hex(INPUT_B)
 
     actual = crypto.bytes_xor(a_bytes, b_bytes)
-    print(actual == expected)
+    expect(actual, EXPECTED)
 
 
 if __name__ == "__main__":
