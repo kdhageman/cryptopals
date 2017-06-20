4fe808937069d1d84f90f29f783aab4ce1636d78
Kaspar Hageman <kaspar.hageman@nedap.com>
2017-06-20 19:50:35 +0200

    Decorate challenge 5

--- challenges/set_1/challenge_5.py
+++ challenges/set_1/challenge_5.py
@@ -1,15 +1,18 @@
 from util import file, crypto, convert
-import pprint
+from challenges.decorator import challenge, expect
+
+plaintext = file.read("set_1/challenge_5")
+EXPECTED = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a2622632427276527" \
+           "2a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
 
+@challenge(5)
 def solve():
-    pp = pprint.PrettyPrinter(indent=4)
-
-    plaintext = file.read("set_1/challenge_5")
     key = b'ICE'
 
     encrypted = crypto.repeating_key(plaintext, key)
-    pp.pprint(convert.to_hex(encrypted))
+
+    expect(convert.to_hex(encrypted), EXPECTED)
 
 
 if __name__ == "__main__":
